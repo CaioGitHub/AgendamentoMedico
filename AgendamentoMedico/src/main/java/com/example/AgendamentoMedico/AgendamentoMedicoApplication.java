7c65bd241fb37df2028a0adec4431343e3538123
Caio Victor <caiopvasconcelos@gmail.com>
2025-09-29 14:19:12 -0300

    Primeiro commit

---
+++ AgendamentoMedico/src/main/java/com/example/AgendamentoMedico/AgendamentoMedicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AgendamentoMedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoMedicoApplication.class, args);
+	}
+
+}
---
+++ AgendamentoMedico/src/test/java/com/example/AgendamentoMedico/AgendamentoMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AgendamentoMedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
