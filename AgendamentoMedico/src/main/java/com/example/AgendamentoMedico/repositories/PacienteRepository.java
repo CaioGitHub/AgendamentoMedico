76d1de5c3d3a4ddc56988b86d02eab5c27b79dab
Caio Victor <caiopvasconcelos@gmail.com>
2025-09-29 16:56:48 -0300

    feature: implementando crud da agenda

--- AgendamentoMedico/src/main/java/com/example/AgendamentoMedico/repositories/PacienteRepository.java
+++ AgendamentoMedico/src/main/java/com/example/AgendamentoMedico/repositories/PacienteRepository.java
@@ -1,12 +1,12 @@
 package com.example.AgendamentoMedico.repositories;
 
-import com.example.AgendamentoMedico.models.Medico;
+import com.example.AgendamentoMedico.models.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MedicoRepository extends JpaRepository<Medico, Long> {
-    Optional<Medico> findByCrm(String crm);
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    Optional<Paciente> findByEmail(String email);
 }
